0ba4d63e3b2ffbc238ac83154d1267c26592edb1
Cole-showers7 <98387486+Cole-showers7@users.noreply.github.com>
2023-05-02 12:11:27 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// var navBtn = document.querySelector(".hamburger-wrapper");
+
+// const animateLines = () => {
+//   document.querySelector("#top").classList.toggle("top-line-animate");
+//   document.querySelector("#middle").classList.toggle("middle-line-animate");
+//   document.querySelector("#bottom").classList.toggle("bottom-line-animate");
+// };
+// navBtn.addEventListener("click", () => {
+//   animateLines();
+// });
